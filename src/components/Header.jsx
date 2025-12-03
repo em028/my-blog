9d9f99829de37e94daa694b783023782bd206264
em028 <hanihanihonihoni@gmail.com>
2025-12-03 22:03:33 +0900

    change Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,13 @@ import './Header.css';
 function Header() {
   return (
     <header className="site-header">
-      <h1 className="site-title"><Link to="/">Termux奮闘記</Link></h1>
+      <h1 className="site-title"><Link to="/">どうしても寝ながらプログラミングしたい！！</Link></h1>
       <nav>
         {/* aタグをLinkコンポーネントに置き換える */}
-        <Link to="/">ホーム</Link>
-        <Link to="/about">概要</Link>
+			<ul>
+				<li><Link to="/">ホーム</Link></li>
+        <li><Link to="/about">概要</Link></li>
+			</ul>
       </nav>
     </header>
   );
